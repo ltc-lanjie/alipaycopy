b2a43e557364bb505fd44c48c0c1ab1eb77752f7
cch <cch@Zhongxing-PC>
2017-07-20 11:46:21 +0800

    1.04
    transaction dao层实现

--- alipaysys/src/com/zhongxing/dao/Impl/TransactionDaoImpl.java
+++ alipaysys/src/com/zhongxing/dao/Impl/TransactionDaoImpl.java
@@ -1,13 +1,16 @@
 package com.zhongxing.dao.Impl;
 
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.zhongxing.dao.DBDao;
 import com.zhongxing.dao.TransactionDao;
 import com.zhongxing.entity.Transaction;
+import com.zhongxing.util.JDBCUtil;
 
 public class TransactionDaoImpl implements TransactionDao {
 
@@ -28,33 +31,39 @@ public class TransactionDaoImpl implements TransactionDao {
 			}
 		}
 		String sql=String.format("select * from transaction %s limit %d,10", order ,page);
-		System.out.println(sql);
+		DBDao dbdao=new DBDaoImpl();
+		list=dbdao.select(sql, Transaction.class);
 		return list;
 	}
 
 	@Override
 	public boolean insert(Transaction transaction) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql =String.format(
+				"insert into transaction(routid,rinid,rmoney,rpaytime,rreport,rorder,rtransstatus) values(%d,%d,%lf,'%s','%s',%d,'%s');",
+				transaction.getRoutid(),transaction.getRinid(),transaction.getRmoney(),transaction.getRpaytime(),transaction.getRreport(),transaction.getRorder(),transaction.getRtransstatus());
+		DBDao dbdao=new DBDaoImpl();
+		return dbdao.insert(sql);
 	}
 
 	@Override
 	public boolean delete(int rid) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql= String.format("update transaction set rtransstatus = '%s' where rid = '%d'", "disable",rid);
+		DBDao dbdao = new DBDaoImpl();
+		return dbdao.update(sql);
 	}
 
 	@Override
 	public boolean update(int rid, String rreport) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql= String.format("update transaction set rreport = '%s' where rid = '%d'", rreport,rid);
+		DBDao dbdao = new DBDaoImpl();
+		return dbdao.update(sql);
 	}
 
 	public static void main(String[] orgs){
 		HashMap<String, String> hm = new HashMap<>();
-        hm.put("test1","test2");
-        hm.put("test1","test4");
-        hm.put("test2", "test4");
+        hm.put(TransactionDao.RID,String.valueOf(678436436));
+       /* hm.put("test1","test4");
+        hm.put("test2", "test4");*/
         TransactionDao td=new TransactionDaoImpl();
         td.select(hm, 0);
 	}
